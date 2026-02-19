e653763b87e0294b7ab3bc9ccd90b3e2372d44c5
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2026-02-20 01:52:00 +0530

    refactor: harden app bootstrap and improve web vitals integration

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import './index.css';
+
+/**
+ * Root DOM container
+ */
+const container = document.getElementById('root');
+
+if (!container) {
+  throw new Error('Root container missing in index.html');
+}
+
+/**
+ * Create React 18 root
+ */
+const root = ReactDOM.createRoot(container);
+
+/**
+ * Render application with StrictMode enabled
+ * to highlight potential problems in development
+ */
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/**
+ * Web Vitals reporting
+ * Can be wired to analytics providers or logging services
+ *
+ * Examples:
+ * reportWebVitals(console.log);
+ * reportWebVitals(sendToAnalytics);
+ */
+if (process.env.NODE_ENV === 'development') {
+  reportWebVitals(console.log);
+} else {
+  reportWebVitals();
+}
